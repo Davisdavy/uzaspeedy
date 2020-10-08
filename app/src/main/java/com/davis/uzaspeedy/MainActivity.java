9a62d04e1a86928d930c010a648307c8db331c14
Davisdavy <davisdavy96@gmail.com>
2020-10-08 16:39:14 +0300

    New fragments layouts with a bottom navigation

--- app/src/main/java/com/davis/uzaspeedy/MainActivity.java
+++ app/src/main/java/com/davis/uzaspeedy/MainActivity.java
@@ -1,27 +1,61 @@
 package com.davis.uzaspeedy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser mCurrentUser;
+    BottomNavigationView bottomNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
 
         //firebase instances
         mAuth = FirebaseAuth.getInstance();
         mCurrentUser = mAuth.getCurrentUser();
+
     }
 
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    openFragment(HomeFragment.newInstance("", ""));
+                    return true;
+                case R.id.navigation_sms:
+                    openFragment(NearYouFragment.newInstance("", ""));
+                    return true;
+                case R.id.navigation_notifications:
+                    openFragment(FarmersFragment.newInstance("", ""));
+                    return true;
+            }
+            return false;
+        }
+    };
     @Override
     protected void onStart() {
         super.onStart();
