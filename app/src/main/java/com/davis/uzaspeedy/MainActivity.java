c871c92b028991d27db013a1fa8dc34865cb6d5d
Davisdavy <davisdavy96@gmail.com>
2020-10-09 16:57:53 +0300

    Add an actionbar with a floating action button

--- app/src/main/java/com/davis/uzaspeedy/MainActivity.java
+++ app/src/main/java/com/davis/uzaspeedy/MainActivity.java
@@ -23,6 +23,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Uzaspeedy"); // for set actionbar title
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         recyclerView = findViewById(R.id.recycler_view);
         mAdapter = new RecyclerAdapter(farmersList);
         RecyclerView.LayoutManager manager = new GridLayoutManager(this, 2);
